f03ad3ec9b87560c5f7543859c83cde0b09e1109
SNIKRO <snikr@bk.ru>
2021-07-05 11:31:59 +0600

    add function in main

--- test_main.py
+++ test_main.py
@@ -1,18 +1,11 @@
-import curses
 from random import randint
+from field_draw import *
 
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
-
+draw_field(20, 60, 0, 0)
 snake = [(4, 10), (4, 9), (4, 8)]
 food = (10, 20)
 
-win.addch(food[0], food[1], '#')
+win_add(food[0], food[1], '#', 1)
 
 score = 0
 
@@ -20,19 +13,19 @@ ESC = 27
 key = curses.KEY_RIGHT
 
 while key != ESC:
-    win.addstr(0, 2, f'Score: {score}')
-    win.timeout(150 - (len(snake)) // 5 + len(snake) // 10 % 120)  # speed increase
+    win_add(0, 2, f'Score: {score}', 2)
+    # draw_field(150 - (len(snake)) // 5 + len(snake) // 10 % 120)  # speed increase
 
     prev_key = key
-    event = win.getch()
-    key = event if event != -1 else prev_key
+    new_key = event()
+    key = new_key if new_key != -1 else prev_key
 
     if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
         key = prev_key
 
     y = snake[0][0]
     x = snake[0][1]
-    win.addstr(0, 11, f"x = {x} y = {y}")
+    win_add(0, 11, f"x = {x} y = {y}", 2)
     if key == curses.KEY_DOWN:
         y += 1
     if key == curses.KEY_UP:
@@ -57,12 +50,12 @@ while key != ESC:
             food = (randint(1, 18), randint(1, 58))
             if food in snake:
                 food = ()
-        win.addch(food[0], food[1], '#')
+        win_add(food[0], food[1], '#', 1)
     else:
         last = snake.pop()
-        win.addch(last[0], last[1], ' ')
+        win_add(last[0], last[1], ' ', 1)
 
-    win.addch(snake[0][0], snake[0][1], '*')
+    win_add(snake[0][0], snake[0][1], '*', 1)
 
-curses.endwin()
+end()
 print(f'Final score : {score}')
