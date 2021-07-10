c12261b1de7a3aad8ff9ff997c79496e82e678ae
SNIKRO <snikr@bk.ru>
2021-07-11 00:16:39 +0600

    Change snake logic and movement

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from movement import Movement
 
 client = Client(20, 60, 0, 0)
 movement = Movement(client)
-snake = [(4, 10), (4, 9), (4, 8)]
+snake = [(4, 8), (4, 9), (4, 10)]
 food = (10, 20)
 
 score = 0
@@ -15,31 +15,27 @@ key = None
 while key != Client.ESC:
     key = client.key_listener()
     x, y = movement.handle_snake_step(snake, key)
-    snake.insert(0, (y, x))  # append 0(n)
-
-    if y in (0, 19):
+    if (y, x) in snake[:-1]:
         break
-    if x in (0, 59):
+    if y in (0, 19) or x in (0, 59):
         break
+    snake.append((y, x))  # append 0(n)
 
-    if snake[0] in snake[1:]:
-        break
-
-    if snake[0] == food:
+    if snake[-1] == food:
         score += 1
-        food = ()
-        while food == ():
+        food = None
+        while food is None:
             food = (randint(1, 18), randint(1, 58))
             if food in snake:
-                food = ()
-
+                food = None
     else:
-        last = snake.pop()
+        tail = snake.pop(0)
+        client.draw(tail[0], tail[1], ' ', 1)
 
     client.draw(0, 2, f'Score: {score}', 2)
     client.draw(food[0], food[1], '#', 1)
-    client.draw(last[0], last[1], ' ', 1)
-    client.draw(snake[0][0], snake[0][1], '*', 1)
+
+    client.draw(snake[-1][0], snake[-1][1], '*', 1)
 
 end_win()
 if key == Client.ESC:
--- movement.py
+++ movement.py
@@ -6,13 +6,15 @@ class Movement:
         self.client = client
         self.prev_key = Client.KEY_RIGHT
 
+
     def handle_snake_step(self, snake, new_key):
         key = new_key if new_key != -1 else self.prev_key
         if key not in [Client.KEY_UP, Client.KEY_LEFT, Client.KEY_RIGHT, Client.KEY_DOWN]:
             key = self.prev_key
 
-        y = snake[0][0]
-        x = snake[0][1]
+        y = snake[-1][0]
+        x = snake[-1][1]
+
         if key == Client.KEY_DOWN:
             y += 1
         if key == Client.KEY_UP:
